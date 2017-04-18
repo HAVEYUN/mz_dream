c0abcaf11f50365d5a9479936d5cb0c8c7b621ef
HAVEYUN <826680069@qq.com>
2017-04-18 11:56:13 +0800

    增加选择器
    自定义选择器，不联动选择器

--- mz_dream/app/src/main/java/zzy/mz_dream/pickerviewdemo/ConditionsSelector.java
+++ mz_dream/app/src/main/java/zzy/mz_dream/pickerviewdemo/ConditionsSelector.java
@@ -6,46 +6,77 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bigkoo.pickerview.OptionsPickerView;
+import com.bigkoo.pickerview.listener.CustomListener;
 
 import java.util.ArrayList;
 
 import zzy.mz_dream.R;
+import zzy.mz_dream.pickerviewdemo.bean.CardBean;
 import zzy.mz_dream.pickerviewdemo.bean.ProvinceBean;
 
 /**
  * Author: mz_dream  PC:MZ
  * Time: 2017/4/17 16:36
  * Email: 826680069@qq.com
- * Instruction: 条件选择器
+ * Instruction: 条件选择器;自定义条件选择器
  */
 public class ConditionsSelector extends AppCompatActivity implements View.OnClickListener {
 
     private ArrayList<ProvinceBean> options1Items = new ArrayList<>();
     private ArrayList<ArrayList<String>> options2Items = new ArrayList<>();
 
-    private Button btn_Options;
+    private ArrayList<CardBean> cardItem = new ArrayList<>();
 
-    private OptionsPickerView pvOptions;
+    /*条件选择器不联动*/
+    private ArrayList<String> food = new ArrayList<>();
+    private ArrayList<String> clothes = new ArrayList<>();
+    private ArrayList<String> computer = new ArrayList<>();
+
+
+    private Button btn_Options, btn_CustomOptions, btn_no_linkage;
+
+    private OptionsPickerView pvOptions, pvCustomOptions, pvNoLinkOptions;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.conditions_activity);
 
-        btn_Options = (Button) findViewById(R.id.btn_Options);
-        btn_Options.setOnClickListener(this);
-
+        //最好等数据加载完毕再初始化并显示，以免数据量大的时候，还未加载完毕就显示，造成APP崩溃。
         initOptionData();
         initOptionPicker();
+
+        getCardData();
+        initCustomOptionPicker();
+
+        getNoLinkData();
+        initNoLinkOptions();
+
+        btn_Options = (Button) findViewById(R.id.btn_Options);
+        btn_CustomOptions = (Button) findViewById(R.id.btn_CustomOptions);
+        btn_no_linkage = (Button) findViewById(R.id.btn_no_linkage);
+
+        btn_Options.setOnClickListener(this);
+        btn_CustomOptions.setOnClickListener(this);
+        btn_no_linkage.setOnClickListener(this);
+
 
     }
 
     @Override
     public void onClick(View view) {
-         if (view.getId() == R.id.btn_Options && pvOptions != null) {
+        if (view.getId() == R.id.btn_Options && pvOptions != null) {
             pvOptions.show(); //弹出条件选择器
+        } else if (view.getId() == R.id.btn_CustomOptions && pvCustomOptions != null) {
+            pvCustomOptions.show();//弹出自定义条件选择器
+
+        } else if (view.getId() == R.id.btn_no_linkage && pvNoLinkOptions != null) {
+            pvNoLinkOptions.show();//不联动数据选择器
+
         }
 
     }
@@ -69,7 +100,7 @@ public class ConditionsSelector extends AppCompatActivity implements View.OnClic
                 .setTitleText("城市选择")
                 .setContentTextSize(20)//设置滚轮文字大小
                 .setDividerColor(Color.GREEN)//设置分割线的颜色
-                .setSelectOptions(0,1)//默认选中项
+                .setSelectOptions(0, 1)//默认选中项
                 .setBgColor(Color.BLACK)
                 .setTitleBgColor(Color.DKGRAY)
                 .setTitleColor(Color.LTGRAY)
@@ -77,7 +108,7 @@ public class ConditionsSelector extends AppCompatActivity implements View.OnClic
                 .setSubmitColor(Color.YELLOW)
                 .setTextColorCenter(Color.LTGRAY)
                 .isCenterLabel(false) //是否只显示中间选中项的label文字，false则每项item全部都带有label。
-                .setLabels("省","市","区")
+                .setLabels("省", "市", "区")
                 .build();
 
         //pvOptions.setSelectOptions(1,1);
@@ -96,9 +127,9 @@ public class ConditionsSelector extends AppCompatActivity implements View.OnClic
          */
 
         //选项1
-        options1Items.add(new ProvinceBean(0,"广东","描述部分","其他数据"));
-        options1Items.add(new ProvinceBean(1,"湖南","描述部分","其他数据"));
-        options1Items.add(new ProvinceBean(2,"广西","描述部分","其他数据"));
+        options1Items.add(new ProvinceBean(0, "广东", "描述部分", "其他数据"));
+        options1Items.add(new ProvinceBean(1, "湖南", "描述部分", "其他数据"));
+        options1Items.add(new ProvinceBean(2, "广西", "描述部分", "其他数据"));
 
         //选项2
         ArrayList<String> options2Items_01 = new ArrayList<>();
@@ -119,6 +150,89 @@ public class ConditionsSelector extends AppCompatActivity implements View.OnClic
         options2Items.add(options2Items_03);
 
         /*--------数据源添加完毕---------*/
+    }
+
+    private void initCustomOptionPicker() {
+        pvCustomOptions = new OptionsPickerView.Builder(this, new OptionsPickerView.OnOptionsSelectListener() {
+            @Override
+            public void onOptionsSelect(int options1, int options2, int options3, View v) {
+                //返回的分别是三个级别的选中位置
+                String tx = cardItem.get(options1).getPickerViewText();
+                btn_CustomOptions.setText(tx);
+            }
+        })
+                .setLayoutRes(R.layout.pickerview_custom_options, new CustomListener() {
+                    @Override
+                    public void customLayout(View v) {
+                        final TextView tvSubmit = (TextView) v.findViewById(R.id.tv_finish);
+                        final TextView tvAdd = (TextView) v.findViewById(R.id.tv_add);
+                        ImageView ivCancel = (ImageView) v.findViewById(R.id.iv_cancel);
+
+                        tvSubmit.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                pvCustomOptions.returnData();
+
+                            }
+                        });
+                        ivCancel.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                pvCustomOptions.dismiss();
+
+                            }
+                        });
+                        tvAdd.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                getCardData();//循环添加数据
+                                pvCustomOptions.setPicker(cardItem);//点击添加立刻更新了
+                            }
+                        });
+                    }
+                })
+                .isDialog(true)
+                .build();
+
+        pvCustomOptions.setPicker(cardItem);//添加数据
+
+    }
+
+
+    public void getCardData() {
+        for (int i = 0; i < 5; i++) {
+            cardItem.add(new CardBean(i, "怎么说" + i));
+
+        }
+    }
+
+    public void initNoLinkOptions() {
+        pvNoLinkOptions = new OptionsPickerView.Builder(this, new OptionsPickerView.OnOptionsSelectListener() {
+            @Override
+            public void onOptionsSelect(int options1, int options2, int options3, View v) {
+
+//                String st = food.get(options1) + clothes.get(options2) + computer.get(options3);
+
+            }
+        }).setTitleText("不联动")
+                .build();
+        pvNoLinkOptions.setNPicker(food, clothes, computer);
+
+    }
+
+    private void getNoLinkData() {
+        food.add("KFC");
+        food.add("MacDonald");
+        food.add("Pizza hut");
+
+        clothes.add("Nike");
+        clothes.add("Adidas");
+        clothes.add("Anima");
+
+        computer.add("ASUS");
+        computer.add("Lenovo");
+        computer.add("Apple");
+        computer.add("HP");
     }
 
 }
--- mz_dream/app/src/main/java/zzy/mz_dream/pickerviewdemo/bean/CardBean.java
+++ mz_dream/app/src/main/java/zzy/mz_dream/pickerviewdemo/bean/CardBean.java
@@ -6,10 +6,11 @@ import com.bigkoo.pickerview.model.IPickerViewData;
  * Author: mz_dream  PC:MZ
  * Time: 2017/4/17 17:20
  * Email: 826680069@qq.com
- * Instruction: please enter class instruction here
+ * Instruction: 功能
  */
 public class CardBean implements IPickerViewData {
     int id;
+
     String cardNo;
 
     public CardBean(int id, String cardNo) {
